c68f6e59bdb237ade6633e926923540a439cabfe
UDDESHYA PANKAJ <Uddeshyapankaj14@gmail.com>
2021-07-03 18:11:31 +0530

    first commit😀😀

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Todo from  "./component/todo";
 
+
 function App() {
   return (
     
        <Todo/>
+     
+
+      
     
   );
 }
---
+++ src/component/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Footer.css"
+const Footer = () =>{
+    return(
+        <div className="main-footer">
+        <div className="conatiner">
+            <div className="row">
+
+            </div>
+        </div>
+            <h1>Made with 💗 by Uddeshya Pankaj </h1>
+        </div>
+    )
+}
+
+export default Footer;
